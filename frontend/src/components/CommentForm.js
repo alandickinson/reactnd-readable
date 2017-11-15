5068bbac70c2fc25174b81d779353066e16c8e73
alandickinson <patrick.alan@gmail.com>
2017-11-15 15:21:35 -0500

    tweak: functional stateless component instead of a class-based component

--- frontend/src/components/CommentForm.js
+++ frontend/src/components/CommentForm.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-class CommentForm extends Component {
-  render() {
-    const { handleSubmit } = this.props
-    return (
-      <form className="comment-form" onSubmit={ handleSubmit }>
-        <h4>Add a comment</h4>
-        <div>
-          <label htmlFor="author">Name</label>
-          <Field name="author" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="body">Comment</label>
-          <Field name="body" component="textarea" type="text" />
-        </div>
-        <button type="submit">Add Comment</button>
-      </form>
-    )
-  }
+let CommentForm = function({handleSubmit}) {
+  return (
+    <form className="comment-form" onSubmit={handleSubmit}>
+      <h4>Add a comment</h4>
+      <div>
+        <label htmlFor="author">Name</label>
+        <Field name="author" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="body">Comment</label>
+        <Field name="body" component="textarea" type="text" />
+      </div>
+      <button type="submit">Add Comment</button>
+    </form>
+  )
 }
 
 CommentForm = reduxForm({
